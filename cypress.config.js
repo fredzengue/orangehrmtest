f379187a83ad5f45349dc8b94b5ab83d11eaa1a5
rostand kinfack <kinfackrostand@gmail.com>
2024-09-04 09:43:40 +0200

    test login work fine

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,11 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = defineConfig({
   projectId: 'ugb3pt',
   e2e: {
-    "specPattern": "**/*.feature",
+    specPattern: "**/*.feature",  // Indique où trouver les fichiers .feature
     setupNodeEvents(on, config) {
+      const cucumber = require('cypress-cucumber-preprocessor').default
       on('file:preprocessor', cucumber())
     },
     
   },
-});
+})
